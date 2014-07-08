8ac9b03c3ba02f0552f8f6f1750cd1b98039eb7c
Siva <sivakumaran.e@gmail.com>
2014-07-08 10:21:55 -0700

    first commit

---
+++ app/controllers/soups_controller.rb
@@ -0,0 +1,42 @@
+class SoupsController < ApplicationController
+	before_action :fetch_soup, only: [:show, :edit, :update, :destroy, :toggle_featured]
+
+	def show
+		respond_to do |format|
+			format.html
+			format.json {render json: @soup}
+		end
+	end
+	
+	def index
+		@soups = Soup.all
+
+		respond_to do |format|
+			format.json { render json: @soups}
+		end
+	end
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	def toggle_featured
+		@soup.toggle!(:featured)
+		flash[:notice] = "Successfully changed featured flag.!"
+		redirect_to @soup
+	end
+
+	private
+
+	def fetch_soup
+		@soup = Soup.find(params[:id])
+	end
+
+end
