e892f14122f68cd65668e9ddb008b53ddf30c474
Siva <sivakumaran.e@gmail.com>
2014-07-08 21:06:57 -0700

    session changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/allcategories", to: redirect("/")
 
   get "/soups/:id/toggle_featured", to: "soups#toggle_featured", as: :toggle_featured
+  get "/secret_soups", to: "soups#secret_soups"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
